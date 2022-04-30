3796b3ea2771bc29beb95252f2403885b688ea4a
Suxrob <example@gmail.com>
2022-04-30 14:33:21 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,63 @@
+'use strict';
+
+//  6 dars
+//  const person = {
+//   firstName: 'suxrob',
+//   lastName: 'muxammadov',
+//   age: 37,
+//   job: 'web dev',
+//   isMaried: true,
+// }
+// console.log(person.job);
+
+// const arr = ['red', 'black', 'yellow'];
+
+// console.log(arr[1]);
+
+// 7 dars
+
+// alert('salom');
+
+// const userAge = confirm('siz 18 yoshdami?');
+// console.log(userAge);
+
+// const userName = prompt('ismingiz nima');
+// console.log(userName);
+
+// const answers = [];
+// answers[0] = prompt('ismingiz nima');
+// answers[1] = prompt('familiyangiz nima');
+// answers[2] = prompt('yoshingiz nima');
+// console.log(answers);
+
+// 8 dars
+
+// const firstName = 'suxrob';
+// const age = 37;
+
+// console.log(`mani ismim ${firstName}, yoshim ${age}`);
+
+// 9 dars
+
+const x = 15,
+y = 10;
+console.log(x+y);
+console.log(x-y);
+console.log(x*y);
+console.log(x/y);
+
+let incr = 10;
+let decr = 10;
+
+console.log(incr);
+console.log(decr);
+
+const q = 15;
+const t = 10;
+
+console.log(q % t);
+
+const isChecked = true;
+  isClose = false;
+
+console.log(isChecked && (isClose || !isChecked));
